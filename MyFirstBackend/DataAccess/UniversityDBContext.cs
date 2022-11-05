5b8e9bc4c9995437953deb408b3b4398a9fb6547
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-11-05 18:15:23 -0300

    termina la clase

---
+++ MyFirstBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstBackend.Models.DataModels;
+
+namespace MyFirstBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext 
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+        }
+
+        //DBSETS -> Tablas de nuestra base de datos
+        //cuando hagamos las migraciones van a usar estos sets para
+        //va a crear tablas con todos los datos de el elemento (User) (la tabla se va a llamar Users)
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ MyFirstBackend/Models/DataModels/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace MyFirstBackend.Models.DataModels
+{
+    public class User : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
