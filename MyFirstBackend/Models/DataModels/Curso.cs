7530050b8f206f6c68b01bf9fa398c55c79e0856
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-11-05 18:21:56 -0300

    ejercicio clase 2

---
+++ MyFirstBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstBackend.Models.DataModels
+{
+    public enum ELevel
+    {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+    public class Curso : BaseEntity
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+        public string LongDescription { get; set; } = string.Empty;
+        public string TargetAudience { get; set; } = string.Empty;
+        public string Goals { get; set; } = string.Empty;
+        public string Requirements { get; set; } = string.Empty;
+        public ELevel Level { get; set; } = ELevel.Basico;
+    }
+}
